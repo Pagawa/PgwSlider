f685ce12d3ecdd3a07d260fe5119f16166c0e577
Roman <rgnevashev@gmail.com>
2016-03-11 08:56:32 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  name: 'pagawa:pgwslider',
+  version: '2.3.1',
+  summary: 'Responsive and lightweight Slider plugin for jQuery / Zepto',
+  git: 'https://github.com/rgnevashev/PgwSlider',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.use(['jquery@1.11.4']);
+  api.addFiles([
+    "pgwslider.css",
+    "pgwslider.js"
+  ], 'client');
+});
